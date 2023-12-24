0b8710a65822d95fa1c790db9176293f4e6b4d9e
burrrnyasheva <burnyasheva.ek@mail.ru>
2023-12-24 20:52:52 +0300

    коммит от Новикова Сергея

--- main.cpp
+++ main.cpp
@@ -29,7 +29,6 @@ public:
             cout << "Номер товара успешно изменен." << endl;
         }
     }
-    //коммит от сережи
     void addProduct(string name, int number) {
         if (products.count(number) != 0) {
             cout << "Товар с номером " << number << " уже существует." << endl;
@@ -43,6 +42,57 @@ public:
 
 int main() {
     setlocale(LC_ALL,"rus");
+
+    //коммит от Новикова Сергея
+    string login, password;
+    cout << "Логин: ";
+    cin >> login;
+    cout << "Пароль: ";
+    cin >> password;
+    if ((login == "admin" && password == "password") || (login == "worker1" && password == "password1")){
+        Shop shop;
+        shop.addProduct("диван", 1);
+        shop.addProduct("кровать", 2);
+
+        int choice;
+        do {
+            cout << endl;
+            cout << "Меню:" << endl;
+            cout << "1. Просмотр товара" << endl;
+            cout << "2. Изменение номера товара" << endl;
+            cout << "3. Добавление нового товара" << endl;
+            cout << "0. Выход" << endl;
+            cout << "Выберите действие: ";
+            cin >> choice;
+            cout << endl;
+            if (choice == 1) {
+                int number;
+                cout << "Введите номер товара для просмотра: ";
+                cin >> number;
+                shop.viewProduct(number);
+            } else if (choice == 2) {
+                int oldNumber, newNumber;
+                cout << "Введите старый номер товара: ";
+                cin >> oldNumber;
+                cout << "Введите новый номер товара: ";
+                cin >> newNumber;
+                shop.changeProductNumber(oldNumber, newNumber);
+            } else if (choice == 3) {
+                string name;
+                int number;
+                cout << "Введите название товара: ";
+                cin >> name;
+                cout << "Введите номер товара: ";
+                cin >> number;
+                shop.addProduct(name, number);
+            }
+
+        } while (choice != 0);
+
+    } else {
+        cout << "Неверный логин или пароль." << endl;
+    }
+
 
     return 0;
 }
